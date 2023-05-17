ddfcc8cb6ed00494762281b874384cad2ea4096b
Mereles Fernando <118017313+warcraftz@users.noreply.github.com>
2023-05-17 23:31:34 +0100

    update some names class an add adm... class

---
+++ BrainGame/src/main/java/isptec/pii/pp2/braingame/Administrador.java
@@ -0,0 +1,7 @@
+package isptec.pii.pp2.braingame;
+
+public class Administrador {
+    public static void Perguntas (){
+        System.out.println("Adim space");
+    }
+}
--- BrainGame/src/main/java/isptec/pii/pp2/braingame/Menus.java
+++ BrainGame/src/main/java/isptec/pii/pp2/braingame/Menus.java
@@ -19,6 +19,9 @@ public class Menus {
                     case 1:
                         Jogador.arrayListLocalSevedPlayer();
                         break;
+                    case 2:
+                        Administrador.Perguntas();
+                        break;
                 }
                 break;
                 
